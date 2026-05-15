56a08b4496c00877ec3be97fc22b7ddd32c0011d
Wadssa Wacemberg <seu-email-do-github@exemplo.com>
2026-05-15 10:36:04 -0300

    Fix: Implementando criptografia Bcrypt

--- src/usuario/usuario.service.ts
+++ src/usuario/usuario.service.ts
@@ -1,19 +1,39 @@
-import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
-import { InjectRepository } from "@nestjs/typeorm";
-import { Repository } from "typeorm";
-import { Usuario } from "./usuario.entity";
-import { UsuarioLogin } from "./usuario.login";
+import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
+import * as bcrypt from 'bcrypt'; // Adicione este import no topo
+import { Usuario } from './usuario.entity';
+import { UsuarioLogin } from './usuario.login';
 
 @Injectable()
 export class UsuarioService {
-    autenticar(usuarioLogin: UsuarioLogin): any {
-      throw new Error("Method not implemented.");
-    }
-    constructor(
-        @InjectRepository(Usuario)
-        private usuarioRepository: Repository<Usuario>,
-    ) { }
+    usuarioRepository: any;
+    
 
+    async autenticar(usuarioLogin: UsuarioLogin): Promise<any> {
+        
+        const buscaUsuario = await this.usuarioRepository.findOne({
+            where: { usuario: usuarioLogin.usuario }
+        });
+
+        if (buscaUsuario) {
+            
+            const senhaValida = await bcrypt.compare(usuarioLogin.senha, buscaUsuario.senha);
+
+            if (senhaValida) {
+                
+                usuarioLogin.id = buscaUsuario.id;
+                usuarioLogin.nome = buscaUsuario.nome;
+                usuarioLogin.foto = buscaUsuario.foto;
+                usuarioLogin.token = 'Bearer ' + 'TOKEN_FAKE_OU_JWT_AQUI'; 
+                
+                return usuarioLogin;
+            }
+        }
+
+      
+        throw new HttpException('Usuário ou senha inválidos!', HttpStatus.UNAUTHORIZED);
+    }
+
+    
     async cadastrar(usuario: Usuario): Promise<Usuario> {
         const buscaUsuario = await this.usuarioRepository.findOne({
             where: { usuario: usuario.usuario }
@@ -22,6 +42,9 @@ export class UsuarioService {
         if (buscaUsuario)
             throw new HttpException("O Usuário já existe!", HttpStatus.BAD_REQUEST);
 
+       
+        usuario.senha = await bcrypt.hash(usuario.senha, 10);
+        
         return await this.usuarioRepository.save(usuario);
     }
 }
