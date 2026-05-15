07d1bd0f85888130157adfed55ca5b25518e6b1f
Wadssa Wacemberg <seu-email-do-github@exemplo.com>
2026-05-15 07:50:17 -0300

    Ajuste: Instalando driver pg e configurando Postgres para o Render

--- app.module.ts
+++ app.module.ts
@@ -7,14 +7,16 @@ import { ProdutoModule } from './src/produto/module/produto.module';
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT),
-      username: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
+      type: 'postgres', 
+      url: process.env.DATABASE_URL, 
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true, 
+      autoLoadEntities: true,
+      extra: {
+        ssl: {
+          rejectUnauthorized: false, 
+        },
+      },
     }),
     CategoriaModule,
     ProdutoModule,
