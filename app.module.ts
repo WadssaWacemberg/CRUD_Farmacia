03a08b22821ae0998ce2ea6a4ff617cebae5a4d1
Wadssa Wacemberg <seu-email-do-github@exemplo.com>
2026-05-15 08:54:59 -0300

    Fix: Resolvendo conflito de merge e estabilizando AppModule com Postgres e Usuarios

--- app.module.ts
+++ app.module.ts
@@ -3,15 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import { CategoriaModule } from './src/categoria/module/categoria.module'; 
 import { ProdutoModule } from './src/produto/module/produto.module';
-<<<<<<< HEAD
-=======
 import { UsuarioModule } from './src/usuario/usuario.module';
->>>>>>> Configurando-o-Projeto
+
 @Module({
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
-<<<<<<< HEAD
       type: 'postgres', 
       url: process.env.DATABASE_URL, 
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
@@ -25,20 +22,7 @@ import { UsuarioModule } from './src/usuario/usuario.module';
     }),
     CategoriaModule,
     ProdutoModule,
-=======
-      type: 'mysql',
-      host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT),
-      username: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true, 
-    }),
-    CategoriaModule,
-    ProdutoModule,
     UsuarioModule,
->>>>>>> Configurando-o-Projeto
   ],
   controllers: [],
   providers: [],
